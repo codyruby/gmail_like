b5b0eb3e2b9bcf4c4c226b027676c28d1751ee80
Yousri <laifayousri@gmail.com>
2019-06-26 12:29:36 +0200

    Modification des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "emails#index"
+  
   resources :tasks, except: [:show]
+  resources :emails
+
+  root to: "emails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
