f408589645e0c07de99a480f3598836f9715d470
Yousri <laifayousri@gmail.com>
2019-06-26 11:50:11 +0200

    Modification des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'emails/index'
+  get 'emails/show'
+  get 'emails/new'
+  get 'emails/create'
+  get 'emails/edit'
+  get 'emails/update'
+  get 'emails/destroy'
   devise_for :users
   root to: "email#index"
   resources :tasks, except: [:show]
