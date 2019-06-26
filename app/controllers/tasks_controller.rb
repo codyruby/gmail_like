1187448b5e78ffa61741b5b6f290417e63b241a8
Yousri <laifayousri@gmail.com>
2019-06-26 11:17:49 +0200

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,61 @@
+class TasksController < ApplicationController
+before_action :authenticate_user!
+  def new
+    @categories = Category.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @category = Category.find(category_params)
+    @task.category = @category
+    @task.save
+    
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    @categories = Category.all
+
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+    flash[:notice] = "Task edited"
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+
+  private
+
+  def task_params
+    params.permit(:title, :deadline, :description)
+  end
+
+  def category_params
+    params.require(:Category)
+  end
+
+end
